c2111d0da3cfad46f2625b218ded5fb5bd980500
Mubeen-Anxari <mubeenansari10001@gmail.com>
2023-08-10 20:04:52 +0500

    daysconditions

---
+++ index.ts
@@ -0,0 +1,10 @@
+var time: number = 23;
+if (time >= 12 && time <= 17) {
+  console.log("Good Afternoon");
+} else if (time >= 18 && time <= 21) {
+  console.log("Good Evening");
+} else if (time >= 21 && time <= 24) {
+  console.log("Good Night");
+} else {
+  console.log("Good Morning");
+}
